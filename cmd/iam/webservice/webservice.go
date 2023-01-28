fae27655f8e6388114aaaf8e963268dc9dfb058a
maria <maria.ic.mihai@gmail.com>
2023-01-28 14:19:31 +0100

    cleanup in webserver

--- cmd/iam/webservice/webservice.go
+++ cmd/iam/webservice/webservice.go
@@ -2,16 +2,20 @@ package webservice
 
 import (
 	"github.com/gin-gonic/gin"
-	"iam/pkg/handlers/middleware"
-	"iam/pkg/handlers/webservice"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"iam/internal/handlers/middleware"
+	"iam/internal/handlers/webservice"
 	"log"
 )
 
-// Todo: add db
-func RunWebservice() error {
+func RunWebservice(ginMode string, _ neo4j.DriverWithContext) error {
 	w := webservice.NewWebservice()
 
 	router := gin.Default()
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+		log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
+	}
+	gin.SetMode(ginMode)
 
 	router.Use(
 		middleware.CorrelationId(),
@@ -23,6 +27,6 @@ func RunWebservice() error {
 
 	router.GET("/ping", w.GetPing)
 
-	log.Println("Starting server on port 8080.")
+	log.Println("Starting server.")
 	return router.Run()
 }
